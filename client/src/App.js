b8ef18e4577091940dbd8b1deff78bc6994080c4
Le Ngoc Tan Toan <tantoan1909@gmail.com>
2022-02-23 11:33:52 +0700

    add package & create router

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const HomePage = React.lazy(() => import("./pages/Home"));
+const LoginPage = React.lazy(() => import("./pages/Login"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Loading App</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import Grid from "@mui/material/Grid";
+import React from "react";
+
+const LoginPage = (_) => {
+  return (
+    <Grid container spacing={0}>
+      <Grid item md={8}>
+      </Grid>
+      <Grid item md={4}>
+        <div >
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default LoginPage;
