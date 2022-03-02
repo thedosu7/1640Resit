43e1b3eaf99eac520fa580c4c8258691d4a134ff
Le Ngoc Tan Toan <tantoan1909@gmail.com>
2022-03-02 10:59:47 +0700

    convert to MVC Laravel

--- client/src/components/NavigationBar/style.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from "@mui/styles";
-
-export const useStyle = makeStyles((theme) => ({
-    logo: {
-      height: "80px",
-      margin: "auto"
-    },
-}))
---
+++ project/database/migrations/2022_03_02_025851_create_idea_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idea_users', function (Blueprint $table) {
+            $table->id();  
+            $table->bigInteger('idea_id')->unsigned();
+            $table->foreign('idea_id')->references('id')->on('ideas');    
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('idea_users');
+    }
+};
