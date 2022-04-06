beced09b03a5a15ac536c71c578f1f8899845b73
NguyenVanHuy1405 <84852270+NguyenVanHuy1405@users.noreply.github.com>
2022-04-06 10:04:05 +0700

    change title index missions

--- project/resources/views/admin/missions/index.blade.php
+++ project/resources/views/admin/missions/index.blade.php
@@ -45,7 +45,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Create new user</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Create new mission</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
